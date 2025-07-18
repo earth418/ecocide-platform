cc445895ea00ba5dd3045654901b16a31ef1424b
Ali Hafez <alihafez42@gmail.com>
2025-07-18 18:06:34 -0400

    made a lil animation

--- flycamera.gd
+++ flycamera.gd
@@ -13,29 +13,40 @@ const LOOKAROUND_SPEED:float = 0.005
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(delta):
+#func _process(delta):
 	# Move the camera based on the key pressed
-	if Input.is_action_pressed("left"):
-		translate(Vector3.LEFT * delta * move_speed)
-	if Input.is_action_pressed("right"):
-		translate(Vector3.RIGHT * delta * move_speed)
-	if Input.is_action_pressed("forward"):
-		translate(Vector3.FORWARD * delta * move_speed)
-	if Input.is_action_pressed("backward"):
-		translate(-Vector3.FORWARD * delta * move_speed)
+	#if Input.is_action_pressed("left"):
+		#translate(Vector3.LEFT * delta * move_speed)
+	#if Input.is_action_pressed("right"):
+		#translate(Vector3.RIGHT * delta * move_speed)
+	#if Input.is_action_pressed("forward"):
+		#translate(Vector3.FORWARD * delta * move_speed)
+	#if Input.is_action_pressed("backward"):
+		#translate(-Vector3.FORWARD * delta * move_speed)
 
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		rot_x -= event.relative.x * LOOKAROUND_SPEED
-		rot_y -= event.relative.y * LOOKAROUND_SPEED
-
-		transform.basis = Basis()
-
-		rotate_object_local(Vector3(0, 1, 0), rot_x)
-		rotate_object_local(Vector3(1, 0, 0), rot_y)
-
-	elif event is InputEventKey:
+	if event is InputEventKey:
 		if event.is_action_released("ui_cancel"):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			get_tree().quit()
+	
+	if event is InputEventMouseButton:
+		if (event as InputEventMouseButton).button_index == 2:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+		#if (event as InputEventMouseButton).button_index == 1:
+			#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	#if event is InputEventKey:
+		#if (event as InputEventKey).keycode == InputEventKey.new().
+	#if event is InputEventMouseMotion:
+		#rot_x -= event.relative.x * LOOKAROUND_SPEED
+		#rot_y -= event.relative.y * LOOKAROUND_SPEED
+#
+		#transform.basis = Basis()
+#
+		#rotate_object_local(Vector3(0, 1, 0), rot_x)
+		#rotate_object_local(Vector3(1, 0, 0), rot_y)
+#
+	#el
--- scene_loader.gd
+++ scene_loader.gd
@@ -7,7 +7,8 @@ var start_position_x := 0.0 #-2000# 000.0
 var start_position_y := 0.0 #3448 #000.0
 
 @export
-var tile_size_meters := 1000.0
+var tile_size_meters := 300.0
+var new_tile_size = tile_size_meters
 
 @export
 var tile_size_pixels := 1000
@@ -18,31 +19,77 @@ var heightmap_data_path: String = ""
 @export
 var ortho_data_path: String
 
+var objects : Array[Node3D] = []
 
+func switch_display(new_display):
+	
+	if new_display == 0:
+		objects.map(func(obj : Node3D): obj.hide)
+		objects[new_display].show()
+	
+#func highlight(new_highlight):
+	#var highlight = 0
+	#match(new_highlight):
+		#0: 
+			#highlight = 0
+
+var zooming_in = -1
+var scale_rate = -1
+var current_scale = 1
+var move_rate = Vector2(0.0, 0.0)
+var elapsed_since_last = 0
+var done = false
+
+func _process(delta: float) -> void:
+	#elapsed_since_last += delta
+	#if elapsed_since_last > 0.5:
+		#elapsed_since_last = 0.0
+	zooming_in -= delta
+	if zooming_in >= 0:
+		current_scale -= scale_rate * delta
+		new_tile_size = current_scale # * tile_size_meters
+		start_position_x += move_rate.x * delta
+		start_position_y += move_rate.y * delta
+		print(new_tile_size)
+		load_images()
+		done = true
+	if zooming_in <= -1 && done:
+		#switch_display(1)
+		$map.hide()
+		$dam.show()
+
+func zoom_in(how_much, where, over_how_long):
+	zooming_in = over_how_long
+	current_scale = tile_size_meters
+	scale_rate = (tile_size_meters - tile_size_meters / how_much) / over_how_long
+	move_rate = Vector2(start_position_x, start_position_y) - where
+	
 func _ready():
+	#objects = [get_node("map"), get_node("dam")]
+	#switch_display(0)
+	load_images()
+	zoom_in(10, Vector2(-30, 23), 5)
 	
-	print("Trying...")
-	
+func load_images():
+
 	var img = Geodot.get_raster_layer(heightmap_data_path).get_image(
 		start_position_x,
 		start_position_y,
-		tile_size_meters,
+		new_tile_size,
 		tile_size_pixels,
 		GeoImage.BILINEAR
 	)
 	var ortho = Geodot.get_raster_layer(ortho_data_path).get_image(
-		start_position_x,
-		start_position_y,
-		tile_size_meters,
-		tile_size_pixels * 4,
+		start_position_x * 4,
+		start_position_y * 4,
+		new_tile_size * 4,
+		tile_size_pixels,
 		GeoImage.NEAREST
 	)
 	
-	print("min:", img.MIN)
-	print("max:", img.MAX)
-	
-	get_node("MeshInstance3D").mesh.surface_get_material(0).set_shader_parameter("heightmap", img.get_image_texture())
-	get_node("MeshInstance3D").mesh.surface_get_material(0).set_shader_parameter("ortho", ortho.get_image_texture())
+	print("Loading images!")
+	get_node("map").mesh.surface_get_material(0).set_shader_parameter("heightmap", img.get_image_texture())
+	get_node("map").mesh.surface_get_material(0).set_shader_parameter("ortho", ortho.get_image_texture())
 	
 	
 	#print("Success!")
